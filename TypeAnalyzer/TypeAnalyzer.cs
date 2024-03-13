a055332f55f0b8b4e2381ca4c36a8ca2861bbbfe
Hladiuc Ionut <hladiuc.ionut001@gmail.com>
2024-03-13 22:17:00 +0200

    Refactored property access methods for increased flexibility.

---
+++ TypeAnalyzer/TypeAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace Type
+{
+    public class Analyzer
+    {
+        public string GetValue(string propertyName)
+        {
+            var property = GetType().GetProperty(propertyName);
+
+            if (property == null)
+            {
+                throw new Exception($"Property '{propertyName}' does not exist in type {GetType().Name}.");
+            }
+
+            object result = property.GetValue(this);
+            return result.ToString();
+        }
+
+        public void UpdateValue(string propertyName, string payload)
+        {
+            PropertyInfo property = GetType().GetProperty(propertyName);
+
+            if (property == null)
+            {
+                throw new Exception($"Property '{propertyName}' does not exist in type {GetType().Name}.");
+            }
+
+            property.SetValue(this, payload);
+        }
+    }
+}
