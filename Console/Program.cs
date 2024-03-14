8dbdfb8a1200e03133a5b75d555fddcac240b4c7
Hladiuc Ionut <hladiuc.ionut001@gmail.com>
2024-03-14 22:52:45 +0200

    Implemented basic input constraints for the provided Product object

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Bar;
 using System;
+using InputGuard;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,26 +12,37 @@ namespace ConsoleBar
     {
         static void Main(string[] args)
         {
-            /*
-            Product product = new Product(name: "LOUIS XIII de Remy Martin", price: "590 LEI", category: "COGNACS", description: "top 1" , stockQuantity : "10");
-            Product secondProduct = new Product(name: "Cuba libre", price: "29 LEI", category: "CLASSIC", description: "top 2", stockQuantity: "22");
-            Product thirdProduct = new Product(name: "Remy Martin XO", price: "115 LEI", category: "COGNACS", description: "top 3", stockQuantity: "13");
-            */
+            Product product = new Product(name: "LOUIS XIII de Remy Martin", price: "590", category: "asd", description: "top 1" , stockQuantity : "asd");
+            Product secondProduct = new Product(name: "Cuba libre", price: "29", category: "2", description: "top 2", stockQuantity: "22");
+            Product thirdProduct = new Product(name: "Remy Martin XO", price: "115", category: "3", description: "top 3", stockQuantity: "13");
 
+                       
             DB.IProductDB db = new DB.ProductDB("test.txt");
-            /*
-            db.Create(product);
-            db.Create(secondProduct);
-            db.Create(thirdProduct);
-            */
-            
-            List<IProduct> list = db.LookupByName("cub");
+
+            if(ProductIntegrityCheck.Validate(product).Count == 0)
+            {
+                db.Create(product);
+            }
+
+            if (ProductIntegrityCheck.Validate(secondProduct).Count == 0)
+            {
+                db.Create(secondProduct);
+            }
+
+            if (ProductIntegrityCheck.Validate(thirdProduct).Count == 0)
+            {
+                db.Create(thirdProduct);
+            }
+
+            List<IProduct> list = db.GetAll();
 
             foreach (IProduct p in list)
             {
                 Console.WriteLine(p.Stringify());
             }
-   
+            
+            
+
         }
     }
 }
---
+++ FieldGuard/FieldGuard.cs
@@ -0,0 +1,73 @@
+﻿using Bar;
+using System;
+using System.Collections.Generic;
+
+namespace InputGuard
+{
+    public class ProductIntegrityCheck
+    {
+        const int nameMinLength = 1;
+        const int nameMaxLength = 32;
+        const int descriptionMinLength = 0;
+        const int descriptionMaxLength = 50;
+
+        public static Dictionary<string, string> Validate(IProduct product)
+        {
+
+            // This method is still under development. Additional constraints may be introduced in future updates.
+
+            Dictionary<string, string> errorList = new Dictionary<string, string>();
+            var properties = product.GetType().GetProperties();
+
+            bool isError;
+            string temporary;
+
+            foreach (var property in properties)
+            {
+                if ( property.Name == "Id" )
+                {
+                    continue;
+                }
+
+                isError = false;
+                temporary = product.GetValue( property.Name );
+
+                if (property.Name == "Price" || property.Name == "StockQuantity" )
+                {
+                    isError = FieldNumberCheck(temporary);
+                }
+
+                if (property.Name == "Name")
+                {
+                    isError = FieldNameCheck(temporary);
+                }
+
+                if (property.Name == "Description")
+                {
+                    isError = FieldDescriptionCheck(temporary);
+                }
+
+                if (isError)
+                {
+                    errorList.Add(property.Name, $"{property.Name} is invalid.");
+                }
+            }
+
+            return errorList;
+        }
+
+        static bool FieldNumberCheck(string payload)
+        {
+            return !int.TryParse(payload, out _);
+        }
+
+        static bool FieldNameCheck(string productName)
+        {
+            return productName.Length <= nameMinLength || productName.Length >= nameMaxLength;
+        }
+        static bool FieldDescriptionCheck(string productDescription)
+        {
+            return productDescription.Length <= descriptionMinLength || productDescription.Length >= descriptionMaxLength;
+        }
+    }
+}
